26ac2a67abb32a8d975e909f6384681051dae8f5
NikitaVadadoriya <125431102+NikitaVadadoriya@users.noreply.github.com>
2024-07-27 16:51:25 +0530

    Update app.py222

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def save_url():
         return jsonify({'error': f'Internal server error: {str(e)}'}), 500
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
